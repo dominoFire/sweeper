63c4e292e3d011f0ef2576ad56ac535fe5d37680
dominofire <fer.aguilar.reyes@gmail.com>
2015-03-31 01:14:21 -0600

    Mejoras al parsing

--- selenium_azure.py
+++ selenium_azure.py
@@ -13,6 +13,9 @@ ddlCurrency = Select(driver.find_element_by_id('waDropdownCurrency'))
 ddlCurrency.select_by_value('USD')
 ddlRegion = Select(driver.find_element_by_id('wa-dropdown-region'))
 
+pricing_list = []
+suse_list = []
+
 for opt in ddlRegion.options:
     current_region = opt.get_attribute('value')
     ddlRegion.select_by_value(current_region)
@@ -23,7 +26,7 @@ for opt in ddlRegion.options:
     div_active = div.find('div', attrs={'class': 'active'}, recursive=False)
     tables = div_active.find_all('table')
     print ('{0} tables: {1}, divs: {2}'.format(current_region, len(tables), len(div_active)))
-    for table in tables:
+    for i, table in enumerate(tables):
         active_pricetags = table.find_all('span', attrs={'class': 'wa-conditionalDisplay active'})
         prices_all = []
         for pricetag in active_pricetags:
@@ -33,12 +36,36 @@ for opt in ddlRegion.options:
         price_hour = prices_all[0::2]
         price_month = prices_all[1::2]
         df = pd.io.html.read_html(str(table), parse_dates=False, flavor='html5lib', infer_types=False)
-        df = df[0]
+        df = pd.DataFrame(df[0])
+
+        title = table.find_previous_sibling('h3')
+        if title:
+            title = title.string
+        else:
+            title = table.parent.find_previous_sibling('h3')
+            if title:
+                title = title.string
+            else:
+                title = 'No inmediate name'
+
+        df['region'] = pd.Series(data=[current_region] * len(df.index))
+        df['description'] = pd.Series(data=[title] * len(df.index))
+
         if len(prices_all) > 1:
             df = df.drop('Price', 1)
             df['price_hour_usd'] = pd.Series(data=price_hour, index=df.index)
             df['price_month_usd'] = pd.Series(data=price_month)
+            pricing_list.append(df)
+        else:
+            suse_list.append(df)
 
-        print (df)
+        print ('{0}: {1}'.format(title, df.shape))
+
+append_df = lambda a, b: a.append(b)
+df_all_pricing = reduce(append_df, pricing_list)
+df_all_suse = reduce(append_df, suse_list)
+
+df_all_pricing.to_csv('azure_pricing_vm_common.csv', index=None, encoding='utf-8')
+df_all_suse.to_csv('azure_pricing_vm_suse.csv', index=None, encoding='utf-8')
 
 driver.close()
