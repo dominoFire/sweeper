64a6b0f7aa1067bc1d3f499147612ecf5093bf79
Fernando Aguilar <fer.aguilar.reyes@gmail.com>
2016-02-06 15:29:14 -0600

    ajustes a los benchmarks

--- scripts/run_perfkit_benchmarks.py
+++ scripts/run_perfkit_benchmarks.py
@@ -5,18 +5,18 @@ import os
 import sys
 
 
-perfkit_home = '/home/microkid/github/PerfKitBenchmarker'
-perfkit_benchmarks = ['coremark', 'bonie++', 'unixbench', 'wrk']
-azure_role_sizes = ['ExtraSmall', 'Small', 'Medium', 'Large', 'ExtraLarge']
+perfkit_home = '/home/microkid/repos/PerfKitBenchmarker'
+perfkit_benchmarks = ['coremark', 'unixbench', 'wrk', 'bonnie++']
+azure_role_sizes = ['ExtraSmall']
 options = {
 	'cloud': 'Azure',
 	'machine_type': None,
 	'benchmarks': None,
-	'archive_bucket': 'gs://sweeper-benchmarks',
-	'os_type': 'ubuntu_container',
-	'json_path': None
+	'archive_bucket': 'gs://sweeper-benchmarks/take2',
+	'os_type': 'debian'
+	#'json_path': None
 }
-n_trials = 30  # Para tener resultados estadisticamente validos
+n_trials = 5  # Para tener resultados estadisticamente validos
 
 
 def parse_opts(opt):
@@ -31,11 +31,9 @@ if __name__ == '__main__':
 				options['machine_type'] = role
 				options['benchmarks'] = bench
 				path_pkb = os.path.join(perfkit_home, 'pkb.py')
-				out_base = 'perfkit_{0}_{1}_{2:02d}'.format(bench, role, (i+1))
-				options['json_path'] = out_base + '.json'				
-
-				print('Testing {0}'.format(out_base))
-
-				cmd = '{0} {1} > {2}.out 2> {2}.err'.format(path_pkb, parse_opts(options), out_base)
+				#out_base = 'perfkit_{0}_{1}_{2:02d}'.format(bench, role, (i+1))
+				#options['json_path'] = out_base + '.json'				
+				#print('Testing {0}'.format(out_base))
+				cmd = '{0} {1}'.format(path_pkb, parse_opts(options))
 				os.system(cmd)
-
+				print(cmd)
